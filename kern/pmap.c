65040ba42f72bde9b483e773649de1708c63c0f8
OS ENVIROMENT <jos@ubuntu.(none)>
2017-06-16 03:06:44 -0700

    lab3 done

--- kern/pmap.c
+++ kern/pmap.c
@@ -154,6 +154,7 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs = boot_alloc(NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -185,6 +186,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	boot_map_region(kern_pgdir, UENVS, ROUNDUP(NENV * sizeof(struct Env), PGSIZE), PADDR(envs), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -531,7 +533,20 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-
+	int va1 = (int)va;
+	int up = ROUNDUP((va1+len), PGSIZE);
+	
+	for (; va1<up; va1 = ROUNDDOWN(va1+PGSIZE, PGSIZE)) {
+		
+		pte_t * pte = pgdir_walk(env->env_pgdir, (void *)va1, 0);
+		if ((!pte)||(~(*pte)&(perm|PTE_P))||va1>=ULIM)
+		{
+			user_mem_check_addr = va1;
+			return -E_FAULT;
+		}
+		
+		
+	}
 	return 0;
 }
 
